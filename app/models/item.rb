e942e1974be9596cf24799f11d08fe8dc4dfd1e1
Kon Ham <info.konham@gmail.com>
2021-11-22 14:33:56 -0500

    Create model validation tests - tests failing

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,8 @@
 class Item < ApplicationRecord
+    # Table Relationship
+    belongs_to :user
+
+    # Data validations
+    validates :quantity, presence: true, numericality: true
+    validates :string, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,10 @@
 class User < ApplicationRecord
+  # Authentication
   has_secure_password
+
+  # Table Relationship
+  has_many :items
+
+  # Data Validations
   validates :email, uniqueness: true
 end
