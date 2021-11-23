4127838b7b272be6acef82febb650421e5c0cd29
Kon Ham <info.konham@gmail.com>
2021-11-23 13:31:16 -0500

    Create text and html erb file for mailer and create mailer method

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,2 +1,11 @@
 class UserMailer < ApplicationMailer
+  default :from => 'me@example.com'
+
+  def send_signup_email(user)
+    @user = current_user
+    mail(
+      :to => @user.email,
+      :subject => 'Your inventory item quantity has reached zero'
+    )
+  end
 end
