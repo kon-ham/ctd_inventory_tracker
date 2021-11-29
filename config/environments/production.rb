9166bb1d6cf5e723289a0638910a62839f845350
ignored-comment <info.konham@gmail.com>
2021-11-29 14:04:49 -0500

    Include configuration for MailGun actionmailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,6 +68,16 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+  :authentication => :plain,
+  :address => "smtp.mailgun.org",
+  :port => 587,
+  :domain => "MYDOMAIN.mailgun.org",
+  :user_name => "postmaster@MYDOMAIN.mailgun.org",
+  :password => "MYPASSWORD"
+}
+
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
