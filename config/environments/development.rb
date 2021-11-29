ddd3265202ee6f2024091dd1b287856358a460aa
ignored-comment <info.konham@gmail.com>
2021-11-29 18:41:06 -0500

    Update changes in smtp to reflect Gmail usage

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,7 +38,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
-  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.delivery_method = :test
   config.action_mailer.smtp_settings = {
     :authentication => :plain,
     :address => "smtp.gmail.com",
