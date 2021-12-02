5b6c1e6e292a6cffa246a89e81801570d57404e0
Kon Ham <info.konham@gmail.com>
2021-12-02 14:20:01 -0500

    Modify home spec test to reflect a status code of redirect

--- spec/requests/home_spec.rb
+++ spec/requests/home_spec.rb
@@ -1,11 +1,10 @@
 require 'rails_helper'
 
-RSpec.describe "Homes", type: :request do
-  describe "GET /index" do
-    it "returns http success" do
-      get "/home/index"
-      expect(response).to have_http_status(:success)
+RSpec.describe 'Homes', type: :request do
+  describe 'GET /index' do
+    it 'redirects to a login page' do
+      get '/'
+      expect(response).to have_http_status(:redirect)
     end
   end
-
 end
